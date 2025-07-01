3c6e8c13fdd6c09c9ee176e8c8287e9215ac2f13
Rayane-G <rayane.walecki@escola.pr.gov.br>
2025-07-01 17:09:33 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+function criaCartao(categoria, pergunta, resposta){
+criaCartao(
+    'CIÊNCIA'
+    'como e feito a fotossintese?'
+    'pelo sol'
+)
+
+criaCartao(
+    'programação'
+    'o que e javasript'
+    'e uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia'
+    'qual a capital do brasil'
+    'brasilia'
+    console.log(categoria, pergunta, resposta)
+)
+}
